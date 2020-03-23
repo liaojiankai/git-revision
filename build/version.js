c8cba3261adeb886419d0cdc56663da6a7bd249b
ernan <ernan@global.com>
2020-03-23 18:25:55 +0800

    v2.1.7

--- build/version.js
+++ build/version.js
@@ -4,10 +4,19 @@ const pathname = path.resolve(__dirname, '../package.json');
 const pkg = JSON.parse(fs.readFileSync(pathname, 'utf-8'));
 let version = pkg.version;
 
+function execSync(cmd) {
+  return require('child_process').execSync(cmd).toString().trim()
+}
+
+const branch = execSync('git rev-parse --abbrev-ref HEAD')
+
+console.log('branch: ', branch);
+
 console.log('version:', version)
 
 const exec = require('child_process').exec;
-let cmdStr = `git commit -m "v${version}" && git push origin && git tag -a "v${version}" -m "${version}" && git push origin --tags`;
+let cmdStr = `git commit -m "v${version}" && git push origin ${branch} && git tag -a "v${version}" -m "${version}" && git push origin --tags`;
+
 exec(cmdStr, function (err, stdout, stderr) {
   console.log('exec:', err, stdout, stderr);
 });

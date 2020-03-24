32e02214b22fc610fdfaec0230a99231411f5285
ernan <ernan@global.com>
2020-03-24 14:38:45 +0800

    v-0.0.3

--- test/index.js
+++ test/index.js
@@ -1,14 +1,24 @@
 const GitRevision = require('../dist/index')
 
 const gitRevision = new GitRevision()
+const {
+  COMMITHASH,
+  VERSION,
+  BRANCH
+} = gitRevision
+
+  console.log(COMMITHASH, VERSION, BRANCH);
+
+
+
 
 console.log('COMMITHASH: ', gitRevision)
-// console.log(
-//   'commithash: ',gitRevision.commithash()
-// )
-// console.log(
-//   'version: ', gitRevision.version()
-// )
-// console.log(
-//   'branch: ', gitRevision.branch()
-// )
+console.log(
+  'commithash: ',gitRevision.commithash()
+)
+console.log(
+  'version: ', gitRevision.version()
+)
+console.log(
+  'branch: ', gitRevision.branch()
+)
